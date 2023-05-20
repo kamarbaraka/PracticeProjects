765d948148474a6ce4b35f18bff7444ca1c242a4
Kb_hacks <kamar254baraka@gmail.com>
2023-05-20 16:46:14 +0300

    v1.0 program to draw random numbers from a fixed range of numbers
    Signed-off-by: Kb_hacks <kamar254baraka@gmail.com>

---
+++ src/main/java/LotteryDrawing.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * a simple console lottery program to draw numbers and win on combination
+ * @author kamar baraka
+ * @version 1.0
+ * @since 20/05/20203 16:00
+ * */
+
+public class LotteryDrawing {
+    public static void main(String[] args){
+        DataCollection collection = new DataCollection();
+        var result = new ResultComputation(collection);
+    }
+}
+
+class DataCollection {
+    int numbersToDraw, highestNumber;
+    public DataCollection(){
+        //create a scanner and attach the input stream to get user input
+        Scanner input = new Scanner(System.in);
+        var prompt = System.out;
+        prompt.println("how many number do you need to draw? ");
+        numbersToDraw = input.nextInt();
+        prompt.println("what is the highest number you can draw");
+        highestNumber = input.nextInt();
+    }
+}
+
+class ResultComputation{
+    /**@param data a DataCollection object.*/
+    public ResultComputation(DataCollection data){
+        //create an array to hold the numbers to draw from
+        int[] numbers = new int[data.highestNumber];
+        //fill the array
+        for (int i = 0; i < numbers.length; i++)
+            numbers[i] = i + 1;
+
+        //creat an array to hold the combination result of the user
+        int[] result = new int[data.numbersToDraw];
+
+        //define a random offset to draw the values from the numbers
+        int randomIndex = (int) (Math.random() * data.highestNumber);
+
+        //fill the result array with random numbers drawn from the numbers
+        for (int i = 0; i < result.length; i++){
+            result[i] = numbers[randomIndex];
+            //move the used number so it cant be repeated
+            numbers[randomIndex] = numbers[data.highestNumber -1];
+            data.highestNumber--;
+        }
+        //sort the result combination and print it
+        Arrays.sort(result);
+        System.out.println("your result is : ");
+        for (Object item : result)
+            System.out.printf("%s ", item);
+    }
+}
