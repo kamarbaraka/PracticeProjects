dbc3b09ebbd23c3338af5d28e83fea94c6a1e62e
Kb_hacks <kamar254baraka@gmail.com>
2023-05-21 17:21:54 +0300

    added a staff class
    Signed-off-by: Kb_hacks <kamar254baraka@gmail.com>

---
+++ src/main/java/Staff.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.time.*;
+/**
+ * a simple java program to store staff information of a company.
+ * the staff object stores the number and information of employees in the company.
+ * the staff object has methods {@methods add} and {@methods getStaff}.
+ * the add method takes an employee object as argument and adds it to itself.
+ * @author kamar baraka
+ * @version 1.0
+ * @since 21/05/2023 17:00
+ * */
+
+public class Staff {
+    private static List<Employee> staff = new ArrayList<>();
+    public static void main(String[] args){
+        //sample code
+        LocalDate date = LocalDate.now();
+        Employee kamar = new Employee("kamar", 10_000.00, date);
+        Staff.add(kamar);
+    }
+    /**@param employee
+     * takes an employee object as parameter*/
+    public static void add(Employee employee){
+        staff.add(employee);
+    }
+    /**@return String.
+     * not fully developed*/
+    public String getStaff(){
+        return "not yet";
+    }
+}
+
+/**
+ * a simple class to store employee information.
+ * it takes the employee name, initial salary and date of employment and stores it.
+ * it has methods {@methods getName}, {@methods getSalary}, {@methods getHireDate} that returns the name,
+ * the salary and the date of hire respectively. It also has methods {@methods raiseSalary} that raises the
+ * salary of the particular employee by a percentage,saves it and returns the resultant value. Method {@methods info} returns an array that contains
+ * the information of the particular employee.
+ * */
+class Employee{
+    private final String name;
+    private double salary;
+    private final LocalDate hireDay;
+
+    /**
+     * @param parsedName the name of the employee
+     * @param  initialSalary the agreed salary
+     * @param parsedHireDay the date the employee was hired. It should be a local date object*/
+    public Employee(String parsedName, double initialSalary, LocalDate parsedHireDay){
+        name = parsedName;
+        salary = initialSalary;
+        hireDay = parsedHireDay;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public LocalDate getHireDay(){
+        return hireDay;
+    }
+/**@param percentage the percentage of the raise*/
+    public double raiseSalary(double percentage){
+        salary = salary * (percentage/100.0);
+        return salary;
+    }
+
+    public String[] info(){
+        Double castedSalary = (Double) salary;
+        return new String[]{name, castedSalary.toString(), hireDay.toString()};
+    }
+}
